0deaece534f999663916c7a11f618a8564454197
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2020-01-30 18:06:39 +0100

    --wip-- [skip ci]

---
+++ buildSrc/src/main/groovy/org/bonitasoft/gradle/plugin/db/DatabaseResourcesConfigurator.groovy
@@ -0,0 +1,27 @@
+package org.bonitasoft.gradle.plugin.db
+
+import org.gradle.api.Project
+
+class DatabaseResourcesConfigurator {
+
+    def static configureDatabaseResources(Project project) {
+        String dbVendor = System.getProperty("db.vendor", "postgres")
+        DatabasePluginExtension extension = project.extensions.getByType(DatabasePluginExtension.class)
+        extension.dbvendor = dbVendor
+        project.logger.quiet("no system property db.url set, using docker to test on $dbVendor")
+        DockerDatabaseContainerTasksCreator.createTasks(project)
+    }
+
+    def static finalizeTasksDependenciesOnDatabaseResources(Project project) {
+        DatabasePluginExtension extension = project.extensions.getByType(DatabasePluginExtension.class)
+        def integrationTestTask = project.tasks.findByName('integrationTest')
+        def uniqueName = "${extension.dbvendor.capitalize()}"
+        def vendorConfigurationTask = project.tasks.findByName("${uniqueName}Configuration")
+        def removeVendorContainer = project.tasks.findByName("remove${uniqueName}Container")
+
+        // Integration tests
+        integrationTestTask.dependsOn(vendorConfigurationTask)
+        removeVendorContainer.mustRunAfter(integrationTestTask)
+    }
+
+}
---
+++ src/test/resources/postgres.sql
@@ -0,0 +1,192 @@
+CREATE TABLE arch_contract_data (
+  tenantid INT8 NOT NULL,
+  id INT8 NOT NULL,
+  kind VARCHAR(20) NOT NULL,
+  scopeId INT8 NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  val TEXT,
+  archiveDate INT8 NOT NULL,
+  sourceObjectId INT8 NOT NULL
+);
+ALTER TABLE arch_contract_data ADD CONSTRAINT pk_arch_contract_data PRIMARY KEY (tenantid, id, scopeId);
+ALTER TABLE arch_contract_data ADD CONSTRAINT uc_acd_scope_name UNIQUE (kind, scopeId, name, tenantid);
+CREATE INDEX idx_acd_scope_name ON arch_contract_data (kind, scopeId, name, tenantid);
+
+CREATE TABLE arch_process_comment(
+  tenantid INT8 NOT NULL,
+  id INT8 NOT NULL,
+  userId INT8,
+  processInstanceId INT8 NOT NULL,
+  postDate INT8 NOT NULL,
+  content VARCHAR(512) NOT NULL,
+  archiveDate INT8 NOT NULL,
+  sourceObjectId INT8 NOT NULL,
+  PRIMARY KEY (tenantid, id)
+);
+
+CREATE INDEX idx1_arch_process_comment on arch_process_comment (sourceobjectid, tenantid);
+CREATE INDEX idx2_arch_process_comment on arch_process_comment (processInstanceId, archivedate, tenantid);
+
+CREATE TABLE arch_document_mapping (
+  tenantid INT8 NOT NULL,
+  id INT8 NOT NULL,
+  sourceObjectId INT8,
+  processinstanceid INT8 NOT NULL,
+  documentid INT8 NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  description TEXT,
+  version VARCHAR(50) NOT NULL,
+  index_ INT NOT NULL,
+  archiveDate INT8 NOT NULL,
+  PRIMARY KEY (tenantid, ID)
+);
+CREATE INDEX idx_a_doc_mp_pr_id ON arch_document_mapping (processinstanceid, tenantid);
+
+CREATE TABLE arch_process_instance (
+  tenantid INT8 NOT NULL,
+  id INT8 NOT NULL,
+  name VARCHAR(75) NOT NULL,
+  processDefinitionId INT8 NOT NULL,
+  description VARCHAR(255),
+  startDate INT8 NOT NULL,
+  startedBy INT8 NOT NULL,
+  startedBySubstitute INT8 NOT NULL,
+  endDate INT8 NOT NULL,
+  archiveDate INT8 NOT NULL,
+  stateId INT NOT NULL,
+  lastUpdate INT8 NOT NULL,
+  rootProcessInstanceId INT8,
+  callerId INT8,
+  sourceObjectId INT8 NOT NULL,
+  stringIndex1 VARCHAR(255),
+  stringIndex2 VARCHAR(255),
+  stringIndex3 VARCHAR(255),
+  stringIndex4 VARCHAR(255),
+  stringIndex5 VARCHAR(255),
+  PRIMARY KEY (tenantid, id)
+);
+CREATE INDEX idx1_arch_process_instance ON arch_process_instance (tenantId, sourceObjectId, rootProcessInstanceId, callerId);
+CREATE INDEX idx2_arch_process_instance ON arch_process_instance (tenantId, processDefinitionId, archiveDate);
+CREATE INDEX idx3_arch_process_instance ON arch_process_instance (tenantId, sourceObjectId, callerId, stateId);
+
+CREATE TABLE arch_flownode_instance (
+  tenantid INT8 NOT NULL,
+  id INT8 NOT NULL,
+  flownodeDefinitionId INT8 NOT NULL,
+  kind VARCHAR(25) NOT NULL,
+  sourceObjectId INT8,
+  archiveDate INT8 NOT NULL,
+  rootContainerId INT8 NOT NULL,
+  parentContainerId INT8 NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  displayName VARCHAR(255),
+  displayDescription VARCHAR(255),
+  stateId INT NOT NULL,
+  stateName VARCHAR(50),
+  terminal BOOLEAN NOT NULL,
+  stable BOOLEAN ,
+  actorId INT8 NULL,
+  assigneeId INT8 DEFAULT 0 NOT NULL,
+  reachedStateDate INT8,
+  lastUpdateDate INT8,
+  expectedEndDate INT8,
+  claimedDate INT8,
+  priority SMALLINT,
+  gatewayType VARCHAR(50),
+  hitBys VARCHAR(255),
+  logicalGroup1 INT8 NOT NULL,
+  logicalGroup2 INT8 NOT NULL,
+  logicalGroup3 INT8,
+  logicalGroup4 INT8 NOT NULL,
+  loop_counter INT,
+  loop_max INT,
+  loopCardinality INT,
+  loopDataInputRef VARCHAR(255),
+  loopDataOutputRef VARCHAR(255),
+  description VARCHAR(255),
+  sequential BOOLEAN,
+  dataInputItemRef VARCHAR(255),
+  dataOutputItemRef VARCHAR(255),
+  nbActiveInst INT,
+  nbCompletedInst INT,
+  nbTerminatedInst INT,
+  executedBy INT8,
+  executedBySubstitute INT8,
+  activityInstanceId INT8,
+  aborting BOOLEAN NOT NULL,
+  triggeredByEvent BOOLEAN,
+  interrupting BOOLEAN,
+  PRIMARY KEY (tenantid, id)
+);
+CREATE INDEX idx_afi_kind_lg2_executedBy ON arch_flownode_instance(logicalGroup2, tenantId, kind, executedBy);
+CREATE INDEX idx_afi_kind_lg3 ON arch_flownode_instance(tenantId, kind, logicalGroup3);
+CREATE INDEX idx_afi_sourceId_tenantid_kind ON arch_flownode_instance (sourceObjectId, tenantid, kind);
+CREATE INDEX idx1_arch_flownode_instance ON arch_flownode_instance (tenantId, rootContainerId, parentContainerId);
+
+CREATE TABLE arch_connector_instance (
+  tenantid INT8 NOT NULL,
+  id INT8 NOT NULL,
+  containerId INT8 NOT NULL,
+  containerType VARCHAR(10) NOT NULL,
+  connectorId VARCHAR(255) NOT NULL,
+  version VARCHAR(50) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  activationEvent VARCHAR(30),
+  state VARCHAR(50),
+  sourceObjectId INT8,
+  archiveDate INT8 NOT NULL,
+  PRIMARY KEY (tenantid, id)
+);
+
+CREATE INDEX idx1_arch_connector_instance ON arch_connector_instance (tenantId, containerId, containerType);
+
+CREATE TABLE arch_ref_biz_data_inst (
+	tenantid BIGINT NOT NULL,
+  	id BIGINT NOT NULL,
+  	kind VARCHAR(15) NOT NULL,
+  	name VARCHAR(255) NOT NULL,
+  	orig_proc_inst_id BIGINT,
+  	orig_fn_inst_id BIGINT,
+  	data_id BIGINT,
+  	data_classname VARCHAR(255) NOT NULL
+);
+CREATE INDEX idx_arch_biz_data_inst1 ON arch_ref_biz_data_inst (tenantid, orig_proc_inst_id);
+CREATE INDEX idx_arch_biz_data_inst2 ON arch_ref_biz_data_inst (tenantid, orig_fn_inst_id);
+ALTER TABLE arch_ref_biz_data_inst ADD CONSTRAINT pk_arch_ref_biz_data_inst PRIMARY KEY (tenantid, id);
+
+CREATE TABLE arch_multi_biz_data (
+	tenantid BIGINT NOT NULL,
+  	id BIGINT NOT NULL,
+  	idx BIGINT NOT NULL,
+  	data_id BIGINT NOT NULL
+);
+ALTER TABLE arch_multi_biz_data ADD CONSTRAINT pk_arch_rbdi_mbd PRIMARY KEY (tenantid, id, data_id);
+ALTER TABLE arch_multi_biz_data ADD CONSTRAINT fk_arch_rbdi_mbd FOREIGN KEY (tenantid, id) REFERENCES arch_ref_biz_data_inst(tenantid, id) ON DELETE CASCADE;
+
+CREATE TABLE arch_data_instance (
+    tenantId INT8 NOT NULL,
+	id INT8 NOT NULL,
+	name VARCHAR(50),
+	description VARCHAR(50),
+	transientData BOOLEAN,
+	className VARCHAR(100),
+	containerId INT8,
+	containerType VARCHAR(60),
+	namespace VARCHAR(100),
+	element VARCHAR(60),
+	intValue INT,
+	longValue INT8,
+	shortTextValue VARCHAR(255),
+	booleanValue BOOLEAN,
+	doubleValue NUMERIC(19,5),
+	floatValue REAL,
+	blobValue BYTEA,
+	clobValue TEXT,
+	discriminant VARCHAR(50) NOT NULL,
+	archiveDate INT8 NOT NULL,
+	sourceObjectId INT8 NOT NULL,
+	PRIMARY KEY (tenantid, id)
+);
+
+CREATE INDEX idx1_arch_data_instance ON arch_data_instance (tenantId, containerId, containerType, archiveDate, name, sourceObjectId);
+CREATE INDEX idx2_arch_data_instance ON arch_data_instance (sourceObjectId, containerId, archiveDate, id, tenantId);
