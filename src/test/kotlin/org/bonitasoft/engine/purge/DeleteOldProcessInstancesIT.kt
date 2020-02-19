cf25a372b1c36361978a8c4fed8e6a6ff36c3704
Danila Mazour <danila.mazour@bonitasoft.com>
2020-02-19 17:57:58 +0100

    Test behaviour on arch_process_instance_table

--- src/test/kotlin/org/bonitasoft/engine/purge/DeleteOldProcessInstancesIT.kt
+++ src/test/kotlin/org/bonitasoft/engine/purge/DeleteOldProcessInstancesIT.kt
@@ -1,6 +1,10 @@
 package org.bonitasoft.engine.purge
 
+import org.assertj.core.api.Assertions.assertThat
+import org.bonitasoft.engine.purge.tables.ArchProcessInstanceTable
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Test
 import org.slf4j.Logger
@@ -9,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.jdbc.core.JdbcTemplate
+import java.util.stream.Collectors
 import kotlin.test.BeforeTest
 
 /**
@@ -67,10 +72,42 @@ class DeleteOldProcessInstancesIT {
 
     @Test
     fun `should delete the records of a process before a timestamp and leave records of other processes and records after the timestamp`() {
-        deleteOldProcessInstances.execute(8663749350673053802, 1000000 /*old date*/, null)
-//        transaction {
-//            Arch
-//        }
+
+        //given
+        val archProcessInstancesBeforePurge = transaction {
+            ArchProcessInstanceTable.selectAll().map(toList)
+        }
+        var otherProcessesCountBeforePurge = archProcessInstancesBeforePurge.count {
+            it.get(3) != "6547377706517145159"
+        }
+
+        //when
+        deleteOldProcessInstances.execute(6547377706517145159, 1923307932000 /*future date*/, 1)
+
+        //then
+        val archProcessInstancesAfterPurge = transaction {
+            ArchProcessInstanceTable.selectAll().map(toList)
+        }
+        var otherProcessesCountAfterPurge = archProcessInstancesAfterPurge.count {
+            it.get(3) != "6547377706517145159"
+        }
+        var processesThatShouldHaveBeenDeletedAtPurge = archProcessInstancesAfterPurge.stream().filter{ it -> it.get(3) == "6547377706517145159" && it.get(4) != "0" }.collect(Collectors.toList())
+
+        assertThat(processesThatShouldHaveBeenDeletedAtPurge.size).isEqualTo(0)
+        assertThat(otherProcessesCountAfterPurge).isEqualTo(otherProcessesCountBeforePurge)
+    }
+
+    val toList = fun(it: ResultRow): List<String?> {
+        return listOf(
+                it[ArchProcessInstanceTable.tenantId].toString(),
+                it[ArchProcessInstanceTable.id].toString(),
+                it[ArchProcessInstanceTable.name],
+                it[ArchProcessInstanceTable.processDefinitionId].toString(),
+                it[ArchProcessInstanceTable.endDate].toString(),
+                it[ArchProcessInstanceTable.stateId].toString(),
+                it[ArchProcessInstanceTable.rootProcessInstanceId].toString(),
+                it[ArchProcessInstanceTable.sourceObjectId].toString()
+        )
     }
 
 }
